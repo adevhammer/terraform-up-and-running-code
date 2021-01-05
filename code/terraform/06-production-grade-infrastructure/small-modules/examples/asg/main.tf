281acbeed3398155ad4cb227d94121a15bac4374
JDevGuru <49580171+zeroday-coder@users.noreply.github.com>
2021-01-05 02:32:52 +0000

    commit

--- code/terraform/06-production-grade-infrastructure/small-modules/examples/asg/main.tf
+++ code/terraform/06-production-grade-infrastructure/small-modules/examples/asg/main.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_version = ">= 0.12, < 0.13"
-}
-
 provider "aws" {
-  region = "us-east-2"
-
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
+  region = "us-east-1"
 }
 
 module "asg" {
